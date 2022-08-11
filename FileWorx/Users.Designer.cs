9fada6f16bec022088be4f726b8e3d6200d761bb
Qais <Qais.Irshaid@sedco-online.com>
2022-08-11 15:45:14 +0300

    test

---
+++ FileWorx/Users.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace FileWorx
+{
+    partial class Users
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.back = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(1, -1);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(828, 301);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 184;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Login Name";
+            this.columnHeader2.Width = 241;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Last Modifier";
+            this.columnHeader3.Width = 202;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Creation Date";
+            this.columnHeader4.Width = 345;
+            // 
+            // back
+            // 
+            this.back.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.back.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.back.Location = new System.Drawing.Point(668, 362);
+            this.back.Name = "back";
+            this.back.Size = new System.Drawing.Size(150, 41);
+            this.back.TabIndex = 1;
+            this.back.Text = "Back";
+            this.back.UseVisualStyleBackColor = true;
+            this.back.Click += new System.EventHandler(this.back_Click);
+            // 
+            // Users
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 415);
+            this.Controls.Add(this.back);
+            this.Controls.Add(this.listView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Users";
+            this.Text = "Users";
+            this.Load += new System.EventHandler(this.Users_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Button back;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+    }
+}
---
+++ FileWorx/Users.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace FileWorx
+{
+    public partial class Users : Form
+    {
+        private static string mainDirectoryPath = Directory.GetCurrentDirectory();
+
+        //removing (/bin/debug) to get to FileWorx as the main folder.
+        private String requiredDirectoryPath = Directory.GetParent(Directory.GetParent(mainDirectoryPath).ToString()).ToString();
+        private readonly string complexSeparator = "%%$$##";
+        public Users()
+        {
+            InitializeComponent();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Users_Load(object sender, EventArgs e)
+        {
+            String path = requiredDirectoryPath + @"\Users\";
+            String[] items = Directory.GetFileSystemEntries(path);
+
+            
+            for (int i = 0; i < items.Length; i++)
+            {
+
+                String[] files = File.ReadAllLines(items[i]);
+                String[] sep = files[0].Split(new string[] { complexSeparator }, StringSplitOptions.None);
+                FileInfo inf = new FileInfo(items[i]);
+                DateTime dt = inf.CreationTime;
+
+                String[] useId = File.ReadAllLines(requiredDirectoryPath + @"\Users\" + sep[3]);
+                String[] sep2 = useId[0].Split(new string[] { complexSeparator }, StringSplitOptions.None);
+
+                ListViewItem item1 = new ListViewItem(sep[0]);
+                item1.Checked = true;
+                item1.SubItems.Add(sep[1]);
+                item1.SubItems.Add(sep2[0]);
+                item1.SubItems.Add(dt.ToString());
+                listView1.Items.Add(item1);
+
+            }
+        }
+
+        private void back_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
